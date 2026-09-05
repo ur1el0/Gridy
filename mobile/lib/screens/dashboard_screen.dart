e06ecc72f44dcaddbeeee6a49454a9ffb0c6f520
ur1el0 <zanoroosc@gmail.com>
2026-09-05 14:17:12 +0800

    fix(mobile): import announcements screen in dashboard

--- mobile/lib/screens/dashboard_screen.dart
+++ mobile/lib/screens/dashboard_screen.dart
@@ -21,6 +21,7 @@ import 'schedule_screen.dart';
 import '../services/storage_service.dart';
 import 'profile_screen.dart';
 import 'my_issues_screen.dart';
+import 'announcements_screen.dart';
 
 class DashboardScreen extends StatefulWidget {
   final DashboardService? dashboardService;
